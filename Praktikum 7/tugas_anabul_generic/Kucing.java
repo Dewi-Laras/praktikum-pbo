4a06629311594cdd99eef6dc626e53b6a9d24412
Dewi-Laras <larasdewi106@gmail.com>
2025-05-13 20:44:58 +0700

    Add files via upload

---
+++ Praktikum 7/tugas_anabul_generic/Kucing.java
@@ -0,0 +1,26 @@
+/*  Nama File    : Kucing.java
+    Deskripsi    : Kelas turunan dari Anabul yang merepresentasikan 
+                   hewan peliharaan jenis kucing. 
+    Pembuat      : Dewi Larasati Mumpuni / 24060123140045
+    Tanggal      : 13 Mei 2025 
+*/
+
+public class Kucing extends Anabul {
+    public Kucing() {
+        super();
+    }
+
+    public Kucing(String nama) {
+        super(nama);
+    }
+
+    @Override
+    public void gerak() {
+        System.out.println(nama + " bergerak : melata");
+    }
+
+    @Override
+    public void suara() {
+        System.out.println(nama + " berbunyi : meong");
+    }
+}
